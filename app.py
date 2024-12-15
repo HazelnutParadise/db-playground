c41b5c409f24dac74c8591b6d2f262c2d60783bc
TimLai666 <43640816+TimLai666@users.noreply.github.com>
2024-12-15 17:59:10 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -67,110 +67,58 @@ async def execute_sql(request: Request):
         database_url = await build_database_url(db_type, version, db_name)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
+   
+    database = databases.Database(database_url)
+    await database.connect()
+
+    response = ""
+    try:
+        if schema_sqls:
+            for schema_sql in schema_sqls:
+                await database.execute(schema_sql)
+            response += f'<h5>Schema executed successfully!</h5><br>'
+
+        if queries:
+            query_num = 0
+            for query in queries:
+                query_num += 1
+                if query.strip():
+                    query_result = await database.fetch_all(query)
+
+                    num_of_rows = len(query_result) if query_result else 0
+                    if query_result:
+                        columns = list(query_result[0].keys())
+                        num_of_columns = len(columns)
+                    else:
+                        columns = []
+                        num_of_columns = 0
+
+                    response += f"<h5>Query #{query_num} [({num_of_rows} rows) * ({num_of_columns} columns)]</h5>"
+                    html_content = ""
+                    if query_result:
+                        # 開始HTML表格
+                        html_content = "<table><tr>" + \
+                            "".join(f"<th>{col}</th>" for col in columns) + "</tr>"
+                        for record in query_result:
+                            record = dict(record)
+                            record_values = list(record.values())
+                            row = "".join(f"<td>{(record_v if record_v is not None else 'null')}</td>" for record_v in record_values)
+                            html_content += f"<tr>{row}</tr>"
+                        html_content += "</table>"
+                    else:
+                        html_content = "<blockquote><strong>No results.</strong></blockquote>"
+                    response += html_content + "<br>"
+
+        if not response:
+            return "No query executed."
+
+        return response
+    except Exception as e:
+        return "error:" + str(e)
     finally:
-        await drop_database(db_type, version, db_name)
-
-    if db_type == "mssql":
-        conn = pyodbc.connect(database_url, autocommit=True)
-        cursor = conn.cursor()
-        response = ""
-        try:
-            if schema_sqls:
-                for schema_sql in schema_sqls:
-                    cursor.execute(schema_sql)
-                conn.commit()
-                response += '<h5>Schema executed successfully!</h5><br>'
-
-            if queries:
-                query_num = 0
-                for query in queries:
-                    query_num += 1
-                    if query.strip():
-                        cursor.execute(query)
-                        query_result = cursor.fetchall()
-
-                        num_of_rows = len(query_result) if query_result else 0
-                        if query_result:
-                            columns = [column[0] for column in cursor.description]
-                            num_of_columns = len(columns)
-                        else:
-                            columns = []
-                            num_of_columns = 0
-
-                        response += f"<h5>Query #{query_num} [({num_of_rows} rows) * ({num_of_columns} columns)]</h5>"
-                        html_content = ""
-                        if query_result:
-                            # 開始HTML表格
-                            html_content = "<table><tr>" + \
-                                "".join(f"<th>{col}</th>" for col in columns) + "</tr>"
-                            for record in query_result:
-                                record_values = list(record)
-                                row = "".join(f"<td>{(record_v if record_v is not None else 'null')}</td>" for record_v in record_values)
-                                html_content += f"<tr>{row}</tr>"
-                            html_content += "</table>"
-                        else:
-                            html_content = "<blockquote><strong>No results.</strong></blockquote>"
-                        response += html_content + "<br>"
-
-            if not response:
-                return "No query executed."
-
-            return response
-        except Exception as e:
-            return "error:" + str(e)
-        finally:
-            cursor.close()
-            conn.close()
-    else:
-        database = databases.Database(database_url)
-        await database.connect()
-
-        response = ""
-        try:
-            if schema_sqls:
-                for schema_sql in schema_sqls:
-                    await database.execute(schema_sql)
-                response += f'<h5>Schema executed successfully!</h5><br>'
-
-            if queries:
-                query_num = 0
-                for query in queries:
-                    query_num += 1
-                    if query.strip():
-                        query_result = await database.fetch_all(query)
-
-                        num_of_rows = len(query_result) if query_result else 0
-                        if query_result:
-                            columns = list(query_result[0].keys())
-                            num_of_columns = len(columns)
-                        else:
-                            columns = []
-                            num_of_columns = 0
-
-                        response += f"<h5>Query #{query_num} [({num_of_rows} rows) * ({num_of_columns} columns)]</h5>"
-                        html_content = ""
-                        if query_result:
-                            # 開始HTML表格
-                            html_content = "<table><tr>" + \
-                                "".join(f"<th>{col}</th>" for col in columns) + "</tr>"
-                            for record in query_result:
-                                record = dict(record)
-                                record_values = list(record.values())
-                                row = "".join(f"<td>{(record_v if record_v is not None else 'null')}</td>" for record_v in record_values)
-                                html_content += f"<tr>{row}</tr>"
-                            html_content += "</table>"
-                        else:
-                            html_content = "<blockquote><strong>No results.</strong></blockquote>"
-                        response += html_content + "<br>"
-
-            if not response:
-                return "No query executed."
-
-            return response
-        except Exception as e:
-            return "error:" + str(e)
-        finally:
-            await database.disconnect()
+        drop_database(db_type, version, db_name)
+        await database.disconnect()
 
         # if db_type == "postgres":
         #     admin_db_url = await build_database_url(db_type, version, "postgres")
