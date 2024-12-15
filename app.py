1ec47012536e2c16660815c5665a4b5e1cb9d130
TimLai666 <43640816+TimLai666@users.noreply.github.com>
2024-12-15 20:16:01 +0800

    modified:   app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from api import set_api
 app = FastAPI()
 set_api(app)
 
-@app.mount("/static", StaticFiles(directory="./frontend/static"), name="static")
+app.mount("/static", StaticFiles(directory="./frontend/static"), name="static")
 
 @app.get('/')
 def index():
